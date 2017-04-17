f20f4f557466aef2d813ce9519bde3b6fd90301a
Njiiii <jousjarvi@gmail.com>
2017-04-18 01:13:38 +0300

    Add files via upload

--- Raffle.java
+++ Raffle.java
@@ -8,19 +8,30 @@ public class Raffle {
     
     protected int num;
     protected int bet;
-    // colours are lists, for now
+    
     protected ArrayList<Integer> reds = 
             new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36));
     
     protected ArrayList<Integer> blacks = 
             new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35));
+    
     //in roulette 0 is usually green, and 00 is 37 because java doesn't think 00 is a thing
     protected int green = 0;
-    protected int ohoh = 37;
+    protected int doublegreen = 37;
     
     private int rouletteWheel(){
         int rng = ThreadLocalRandom.current().nextInt(0, 37);
+        //int rng = 10;
+        //System.out.println(rng);
+       
         return rng;
+    }
+    
+    //Spin is used to check if rouletteWheel() works
+    public void spin(){
+        int a = rouletteWheel();
+        System.out.println(a);
+        
     }
     
     public void gamble(int num, int bet){
@@ -28,9 +39,22 @@ public class Raffle {
         this.num = num;
         this.bet = bet;
         
-        System.out.println(slot);
+        if (reds.contains(slot)){
+            System.out.println("Red, " + slot);
+        }
+        else if (blacks.contains(slot)){
+            System.out.println("Black, " + slot);
+        }
+        else if (slot == green){
+            System.out.println("Zero");
+        }
+        else if (slot == doublegreen){
+            System.out.println("Double zero");
+        }
         
         if (num == slot){
+            bet = bet * 37;
+            System.out.println("Ammount won: " + bet + "$");
             System.out.println("WIN");
         }
         else {
